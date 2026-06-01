9134622fe4b52b835852533babebb3af0deadfb7
Fabriciana Lima <fabricianacorreialima@gmail.com>
2026-06-01 10:39:52 -0300

    Iniciando o projeto de Delivery de Alimentos

---
+++ src/main/java/com/generation/nutriGo/NutriGoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.nutriGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/nutriGo/NutriGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.nutriGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
